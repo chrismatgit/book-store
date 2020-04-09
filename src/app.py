fa2f3ec6284782b6c2dc399c11d7a48cce661728
chrismatgit <christianmatabaro@gmail.com>
2020-04-09 05:56:11 +0200

    create two functions. one to delete data and the other to update data from the table

--- src/app.py
+++ src/app.py
@@ -26,8 +26,6 @@ def insert_data(item, quantity, price):
     # At the end close the connection
     connection.close()
 
-insert_data("Juice", 50, 8)
-
 def view_data():
     ''' Function to view data from the database'''
     # create a connection
@@ -43,4 +41,34 @@ def view_data():
     # returning the result of the function as a list
     return res
 
-print(view_data())
+
+def delete_data(item):
+    ''' Function to delete data from the database'''
+    # create a connection
+    connection = sqlite3.connect("lite.db")
+    # create a cursor
+    cursor = connection.cursor()
+    # Sql comment goes in the function execute
+    cursor.execute("DELETE FROM store WHERE item=?", (item,))
+    # Commit or execute the query
+    connection.commit()
+    # At the end close the connection
+    connection.close()
+
+def update_data(quantity,price,item):
+    ''' Function to delete data from the database'''
+    # create a connection
+    connection = sqlite3.connect("lite.db")
+    # create a cursor
+    cursor = connection.cursor()
+    # Sql comment goes in the function execute
+    cursor.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity,price,item,))
+    # Commit or execute the query
+    connection.commit()
+    # At the end close the connection
+    connection.close()
+
+update_data(70, 9,"Juice") # update data from the database
+# delete_data("milk") # delete data
+print(view_data())   
+# insert_data("milk", 20, 10) #insert data into the table
