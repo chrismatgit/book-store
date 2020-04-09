cc7c5eb52adcdead454ef5c705284c6e8f59a612
chrismatgit <christianmatabaro@gmail.com>
2020-04-09 05:35:04 +0200

    create 3 function to create a table, insert and view data

--- src/app.py
+++ src/app.py
@@ -1,16 +1,46 @@
 import sqlite3
 
-# create a connection
-connection = sqlite3.connect("lite.db")
+def create_table():
+    ''' Function to insert create table store into the database'''
+    # create a connection
+    connection = sqlite3.connect("lite.db")
+    # create a cursor
+    cursor = connection.cursor()
+    # Sql comment goes in the function execute
+    cursor.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)")
+    # Commit or execute the query
+    connection.commit()
+    # At the end close the connection
+    connection.close()
 
-# create a cursor
-cursor = connection.cursor()
+def insert_data(item, quantity, price):
+    ''' Function to insert data into the database'''
+    # create a connection
+    connection = sqlite3.connect("lite.db")
+    # create a cursor
+    cursor = connection.cursor()
+    # Sql comment goes in the function execute
+    cursor.execute("INSERT INTO store VALUES(?,?,?)",(item,quantity,price))
+    # Commit or execute the query
+    connection.commit()
+    # At the end close the connection
+    connection.close()
 
-# Sql comment goes in the function execute
-cursor.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)")
+insert_data("Juice", 50, 8)
 
-# Commit or execute the query
-connection.commit()
+def view_data():
+    ''' Function to view data from the database'''
+    # create a connection
+    connection = sqlite3.connect("lite.db")
+    # create a cursor
+    cursor = connection.cursor()
+    # Sql comment goes in the function execute
+    cursor.execute("SELECT*FROM store")
+    # store the result into a variable
+    res = cursor.fetchall()
+    # At the end close the connection
+    connection.close()
+    # returning the result of the function as a list
+    return res
 
-# At the end close the connection
-connection.close()
+print(view_data())
