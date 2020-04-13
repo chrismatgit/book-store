c9431ab73fc1d2bec68190a69cd1153e21243946
chrismatgit <christianmatabaro@gmail.com>
2020-04-13 08:51:05 +0200

    finish to turning the application into OPP

--- src/backend.py
+++ src/backend.py
@@ -7,88 +7,60 @@ class Database:
     def __init__(self):
         ''' Constructor to connect to the database'''
         # create a connection
-        connection = sqlite3.connect("books.db")
+        self.connection = sqlite3.connect("books.db")
         # create a cursor
-        cursor = connection.cursor()
+        self.cursor = self.connection.cursor()
         # Sql comment goes in the method execute
-        cursor.execute(
+        self.cursor.execute(
             "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
         # Commit or execute the query
-        connection.commit()
-        # At the end close the connection
-        connection.close()
+        self.connection.commit()
 
     def insert_data(self, title, author, year, isbn):
-        ''' method to insert data into the database'''
-        # create a connection
-        connection = sqlite3.connect("books.db")
-        # create a cursor
-        cursor = connection.cursor()
-        # Sql comment goes in the method execute
-        cursor.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",
-                       (title, author, year, isbn))
+        ''' Function to insert data into the database'''
+        # Sql comment goes in the Function execute
+        self.cursor.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",
+                            (title, author, year, isbn))
         # Commit or execute the query
-        connection.commit()
-        # At the end close the connection
-        connection.close()
+        self.connection.commit()
 
     def view_data(self):
-        ''' method to view data from the database'''
-        # create a connection
-        connection = sqlite3.connect("books.db")
-        # create a cursor
-        cursor = connection.cursor()
-        # Sql comment goes in the method execute
-        cursor.execute("SELECT * FROM book")
+        ''' Function to view data from the database'''
+        # Sql comment goes in the Function execute
+        self.cursor.execute("SELECT * FROM book")
         # store the result into a variable by returning a tuple
-        res = cursor.fetchall()
-        # At the end close the connection
-        connection.close()
-        # returning the result of the method as a list
+        res = self.cursor.fetchall()
+        # returning the result of the Function as a list
         return res
 
     def search_data(self, title='', author='', year='', isbn=''):
-        ''' method to search data from the database'''
-        # create a connection
-        connection = sqlite3.connect("books.db")
-        # create a cursor
-        cursor = connection.cursor()
-        # Sql comment goes in the method execute
-        cursor.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",
-                       (title, author, year, isbn))
+        ''' Function to search data from the database'''
+        # Sql comment goes in the Function execute
+        self.cursor.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",
+                            (title, author, year, isbn))
         # store the result into a variable by returning a tuple
-        res = cursor.fetchall()
+        res = self.cursor.fetchall()
         # At the end close the connection
-        connection.close()
-        # returning the result of the method as a list
         return res
 
     def delete_data(self, id):
-        ''' method to delete data from the database'''
-        # create a connection
-        connection = sqlite3.connect("books.db")
-        # create a cursor
-        cursor = connection.cursor()
-        # Sql comment goes in the method execute
-        cursor.execute("DELETE FROM book WHERE id=?", (id,))
+        ''' Function to delete data from the database'''
+        # Sql comment goes in the Function execute
+        self.cursor.execute("DELETE FROM book WHERE id=?", (id,))
         # Commit or execute the query
-        connection.commit()
-        # At the end close the connection
-        connection.close()
+        self.connection.commit()
 
     def update_data(self, id, title, author, year, isbn):
-        ''' method to update data from the database'''
-        # create a connection
-        connection = sqlite3.connect("books.db")
-        # create a cursor
-        cursor = connection.cursor()
-        # Sql comment goes in the method execute
-        cursor.execute(
+        ''' Function to update data from the database'''
+        # Sql comment goes in the Function execute
+        self.cursor.execute(
             "UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
         # Commit or execute the query
-        connection.commit()
-        # At the end close the connection
-        connection.close()
+        self.connection.commit()
+
+    def __del__(self):
+        ''' A special methode to close the connection '''
+        self.connection.close()
 
     # connect_db()
     # insert_data("The Sum", "John Smith", 1950, 1349987599)
